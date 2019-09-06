077effa647f52460b5ebec0572db0b710ce9e46a
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-09-06 04:29:47 -0700

    Added number type to all TweenFunction parameters.

--- src/components/tweens/TweenFunctions.ts
+++ src/components/tweens/TweenFunctions.ts
@@ -5,7 +5,7 @@ export namespace TweenFunctions {
 	 * @param c relative target value
 	 * @param d duration
 	 */
-	export function linear(t, b, c, d) {
+	export function linear(t: number, b: number, c: number, d: number) {
 		return c*t/d + b;
 	}
 
@@ -15,116 +15,116 @@ export namespace TweenFunctions {
 	 * @param c relative target value
 	 * @param d duration
 	 */
-	export function easeInQuad(t, b, c, d) {
+	export function easeInQuad(t: number, b: number, c: number, d: number) {
 		t /= d;
 		return c*t*t + b;
 	}
 
-	export function easeOutQuad(t, b, c, d) {
+	export function easeOutQuad(t: number, b: number, c: number, d: number) {
 		t /= d;
 		return -c * t*(t-2) + b;
 	};
 
-	export function easeInOutQuad(t, b, c, d) {
+	export function easeInOutQuad(t: number, b: number, c: number, d: number) {
 		t /= d/2;
 		if (t < 1) return c/2*t*t + b;
 		t--;
 		return -c/2 * (t*(t-2) - 1) + b;
 	}
 
-	export function easeInCubic(t, b, c, d) {
+	export function easeInCubic(t: number, b: number, c: number, d: number) {
 		t /= d;
 		return c*t*t*t + b;
 	}
 
-	export function easeOutCubic(t, b, c, d) {
+	export function easeOutCubic(t: number, b: number, c: number, d: number) {
 		t /= d;
 		t--;
 		return c*(t*t*t + 1) + b;
 	}
 
-	export function easeInOutCubic(t, b, c, d) {
+	export function easeInOutCubic(t: number, b: number, c: number, d: number) {
 		t /= d/2;
 		if (t < 1) return c/2*t*t*t + b;
 		t -= 2;
 		return c/2*(t*t*t + 2) + b;
 	}
 
-	export function easeInQuart(t, b, c, d) {
+	export function easeInQuart(t: number, b: number, c: number, d: number) {
 		t /= d;
 		return c*t*t*t*t + b;
 	}
 
-	export function easeOutQuart(t, b, c, d) {
+	export function easeOutQuart(t: number, b: number, c: number, d: number) {
 		t /= d;
 		t--;
 		return -c * (t*t*t*t - 1) + b;
 	}
 
-	export function easeInOutQuart(t, b, c, d) {
+	export function easeInOutQuart(t: number, b: number, c: number, d: number) {
 		t /= d/2;
 		if (t < 1) return c/2*t*t*t*t + b;
 		t -= 2;
 		return -c/2 * (t*t*t*t - 2) + b;
 	}
 
-	export function easeInQuint(t, b, c, d) {
+	export function easeInQuint(t: number, b: number, c: number, d: number) {
 		t /= d;
 		return c*t*t*t*t*t + b;
 	}
 
-	export function easeOutQuint(t, b, c, d) {
+	export function easeOutQuint(t: number, b: number, c: number, d: number) {
 		t /= d;
 		t--;
 		return c*(t*t*t*t*t + 1) + b;
 	}
 
-	export function easeInOutQuint(t, b, c, d) {
+	export function easeInOutQuint(t: number, b: number, c: number, d: number) {
 		t /= d/2;
 		if (t < 1) return c/2*t*t*t*t*t + b;
 		t -= 2;
 		return c/2*(t*t*t*t*t + 2) + b;
 	}
 
-	export function easeInSine(t, b, c, d) {
+	export function easeInSine(t: number, b: number, c: number, d: number) {
 		return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
 	}
 
-	export function easeOutSine(t, b, c, d) {
+	export function easeOutSine(t: number, b: number, c: number, d: number) {
 		return c * Math.sin(t/d * (Math.PI/2)) + b;
 	}
 
-	export function easeInOutSine(t, b, c, d) {
+	export function easeInOutSine(t: number, b: number, c: number, d: number) {
 		return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
 	}
 
-	export function easeInExpo(t, b, c, d) {
+	export function easeInExpo(t: number, b: number, c: number, d: number) {
 		return c * Math.pow( 2, 10 * (t/d - 1) ) + b;
 	}
 
-	export function easeOutExpo(t, b, c, d) {
+	export function easeOutExpo(t: number, b: number, c: number, d: number) {
 		return c * ( -Math.pow( 2, -10 * t/d ) + 1 ) + b;
 	}
 
-	export function easeInOutExpo(t, b, c, d) {
+	export function easeInOutExpo(t: number, b: number, c: number, d: number) {
 		t /= d/2;
 		if (t < 1) return c/2 * Math.pow( 2, 10 * (t - 1) ) + b;
 		t--;
 		return c/2 * ( -Math.pow( 2, -10 * t) + 2 ) + b;
 	}
 
-	export function easeInCirc(t, b, c, d) {
+	export function easeInCirc(t: number, b: number, c: number, d: number) {
 		t /= d;
 		return -c * (Math.sqrt(1 - t*t) - 1) + b;
 	}
 
-	export function easeOutCirc(t, b, c, d) {
+	export function easeOutCirc(t: number, b: number, c: number, d: number) {
 		t /= d;
 		t--;
 		return c * Math.sqrt(1 - t*t) + b;
 	}
 
-	export function easeInOutCirc(t, b, c, d) {
+	export function easeInOutCirc(t: number, b: number, c: number, d: number) {
 		t /= d/2;
 		if (t < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
 		t -= 2;
