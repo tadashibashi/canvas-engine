0106fe5cff26f18945f741d268afbcccd13e26c5
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-09-06 04:31:31 -0700

    Testing

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,46 @@ import { Component } from './components/Component';
 import { ComponentManager } from './components/ComponentManager';
 import { StateMachine } from './components/states/StateMachine';
 import { GameTime } from './core/GameTime';
+import { Game } from './components/game/Game';
+import { GameObject } from './components/gameobjects/GameObject';
 
 
+let game = new Game({
+	canvasID: 'canvas',
+	height: 180,
+	width: 320,
+	pixelated: false
+});
 
-let joe = {};
-let c = new StateMachine<'game' | 'title'>(joe);
-c.add('title')
-	.onEnter(() => {
-		console.log('hello component');
-	})
-	.onUpdate((gameTime, seconds) => {
-		console.log("I've been in this state for:", seconds, "seconds.");
-	});
+let go = new GameObject();
+let go2 = new GameObject();
+go2.transform.addChild(go);
+game.awake();
+// let joe = {};
+// let c = new StateMachine<'game' | 'title'>(joe);
+// c.add('title')
+// 	.onEnter(() => {
+// 		console.log('hello component');
+// 	})
+// 	.onUpdate((gameTime, seconds) => {
+// 		console.log("I've been in this state for:", seconds, "seconds.");
+// 	});
 
-let m = new ComponentManager();
+// let m = new ComponentManager();
 
-m.add(c);
-m.load();
+// m.add(c);
+// m.load();
 
 let gameTime = new GameTime();
-m.get(StateMachine).start('title');
 setInterval(main, 100);
+
 let time = 0;
 function main() {
 	time += 100;
+	if (time > 1000) {
+		go2.transform.removeChild(go);
+	}
 	gameTime.update(time);
-	m.update(gameTime);
+	game.update(gameTime);
+	game.draw(gameTime);
 }
