6df02e84272af977b52810047c47d0fd5dd40494
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-09-06 19:52:19 -0700

    Added ComponentTemplate for easy duplication

---
+++ src/components/templates/ComponentTemplate.ts
@@ -0,0 +1,19 @@
+import { Component } from '../Component';
+import { GameTime } from '../../core/GameTime';
+import { Game } from '../game/Game';
+
+export class NewComponent extends Component {
+
+	constructor() {
+		super(0); // Update Order
+	}
+
+	awake() {
+		// Game.engine.services.get()
+	}
+
+	update(gameTime: GameTime) {
+		// Update Logic
+
+	}
+}
