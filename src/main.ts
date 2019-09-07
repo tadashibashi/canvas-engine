a61a6f7cf4268ca4d4e1e7e77c2992956c58c390
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-09-07 10:02:21 -0700

    Some test updates

--- src/main.ts
+++ src/main.ts
@@ -10,27 +10,12 @@ window.onload = (ev: Event) => {
 		canvasID: 'canvas',
 		height: 180,
 		width: 320,
-		pixelated: false
+		pixelated: false,
+		backgroundColor: 'black'
 	});
 
-	let go = new GameObject();
-	let go2 = new GameObject();
-	go2.transform.addChild(go);
+
 	game.awake();
-	// let joe = {};
-	// let c = new StateMachine<'game' | 'title'>(joe);
-	// c.add('title')
-	// 	.onEnter(() => {
-	// 		console.log('hello component');
-	// 	})
-	// 	.onUpdate((gameTime, seconds) => {
-	// 		console.log("I've been in this state for:", seconds, "seconds.");
-	// 	});
-
-	// let m = new ComponentManager();
-
-	// m.add(c);
-	// m.load();
 
 	let gameTime = new GameTime();
 	setInterval(main, 16.67);
@@ -38,9 +23,6 @@ window.onload = (ev: Event) => {
 	let time = 0;
 	function main() {
 		time += 16.67;
-		if (time > 1000) {
-			go2.transform.removeChild(go);
-		}
 		gameTime.update(time);
 		game.update(gameTime);
 		game.draw(gameTime);
