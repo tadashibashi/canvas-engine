c70ebf2ad7b7670d3ceda4e15a4e1558998905c2
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-09-08 02:49:28 -0700

    Fixed Delegate's search functionality, added comments.

--- src/core/Delegate.ts
+++ src/core/Delegate.ts
@@ -1,28 +1,63 @@
-export class Delegate<F extends (...any: any[]) => void> { 
+/**
+ * A handle for others to subscribe callback functions to, to listen in for an event.
+ * When the event occurs, use Delegate.send() to let every subscriber know the thing has just happened.
+ * @type S A tuple of the type parameters
+ */
+export class Delegate<F extends (...any: any[])=>void> { 
 	constructor() {}
-	private handle: [any, F][] = [];
-	subscribe(context: any, fn: F) {
-		this.handle.push([context, fn]);
+
+	/**
+	 * The inner collection of event handler functions with the contexts
+	 */
+	private handle: {context: any, callback: F}[] = [];
+
+	/**
+	 * Subscribe a callback handler to the delegate
+	 * @param context The contextual 'this' that the function will operate by
+	 * @param callback The callback function to add that will trigger when this event occurs 
+	 * (when Delegate.send(...params[]) is called)
+	 */
+	subscribe(context: any, callback: F): void {
+		// Check if this callback handler already exists on the handle
+		let handle = this.handle;
+		for (let i=0; i<handle.length; i++) {
+			// if so, exit this function
+			if (context === handle[i].context && callback === handle[i].callback) {			
+				return;
+			}
+		}
+		// if not, then add it to the handle!
+		this.handle.push({ context: context, callback: callback });
 	}
-	unsubscribe(context: any, fn: F) {
-		let index = this.handle.map((h) => {return h[0]}).indexOf(context);
-		let index2 = this.handle.map((h) => {return h[1]}).indexOf(fn);
-		if (index === index2 && index >= 0) {
-			this.handle.splice(index, 1);
-		} else {
-			console.log('Does not exist on this delegate handle');
+	
+	/**
+	 * Remove a subscription to this Delegate
+	 * @param context The contextual 'this' that the function operates by
+	 * @param callback The callback function to remove
+	 */
+	unsubscribe(context: any, callback: F): void {
+		const handle = this.handle;
+		for (let i=0; i<handle.length; i++) {
+			if (context === handle[i].context && callback === handle[i].callback) {
+				this.handle.splice(i, 1);
+				return;
+			}
 		}
 	}
+
 	/**
-	 * 
+	 * Callback to every subscriber to this Delegate. Let them know the thing just happened!
 	 * @param params Make sure to send the same parameters as the signature of the delegate
 	 */
-	send(...params: any[]) {
+	send(...params: any[]): void {
 		this.handle.forEach((t) => {
-			t[1].call(t[0], ...params);
+			t.callback.call(t.context, ...params);
 		});
 	}
 
+	/**
+	 * Clears this delegate of all subscribers' callbacks
+	 */
 	unsubscribeAll() {
 		this.handle = [];
 	}
