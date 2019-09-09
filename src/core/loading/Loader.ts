593305e1894d4eef6d0c0621cc492b8405326a80
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-09-09 16:57:58 -0700

    Adjusted Loader.ts Delegate to allow any parameters to be passed into it.

--- src/core/loading/Loader.ts
+++ src/core/loading/Loader.ts
@@ -4,7 +4,7 @@ export abstract class Loader {
 	/**
 	 * An event that fires when all assets from this loader are finished loading
 	 */
-	readonly onLoadFinish = new Delegate<()=>void>();
+	readonly onLoadFinish = new Delegate<(...any: any[])=>void>();
 
 	/**
 	 * Set this for debug logging purposes
