6fde8600d36beb8b84abac96314461aabc19d464
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-09-10 20:58:26 -0700

    FMOD Loader now receives a config to load.

--- src/core/loading/fmodstudio/FMODLoader.ts
+++ src/core/loading/fmodstudio/FMODLoader.ts
@@ -1,97 +1,65 @@
-import { StateMachine } from '../../../components/states/StateMachine';
-import { IFMOD, FMOD, CHECK_RESULT } from '../../../libraries/IFMOD/IFMOD';
+import { IFMOD, FMOD, CHECK_RESULT, parseGUID } from '../../../libraries/IFMOD/IFMOD';
 import { Loader } from '../Loader';
 import { Delegate } from '../../Delegate';
+import { DelegateGroup } from '../../DelegateGroup';
+import { FMODPreloadFileData, FMODLoaderConfig } from './FMODLoaderTypes';
+
+// In index.html make sure that fmodstudio.js is loaded in a script before this library's modules
 declare const FMODModule: (fmodObject: any)=>void;
 
-interface FMODStudioConfig {
-	preloadFiles: string[];
-	baseDir: string;
-	totalMemory: number;
-	studioInitFlags: IFMOD.STUDIO_INITFLAGS;
-	initFlags: IFMOD.INITFLAGS;
-	initBanksLoad: string[];
-}
-
-interface FMODPreloadFileData {
-	/**
-	 * Where in the virtual directory you want this file to go. 
-	 * Use '/' for root directory.
-	 */
-	directory: string;
-	/**
-	 * filename with extension. e.g. 'Master Bank.bank'
-	 */
-	fileNames: string[];
-	/**
-	 * URL relative to the assetBaseURL set on FMODLoader's construction.
-	 * Do not fill in, or put an empty string if in the assetBaseURL root.
-	 * e.g. 'banks/' // if the folder structure of the assetBaseURL includes a 'banks' folder.
-	 */
-	url?: string;
-
-	/**
-	 * Will the file be readable? (default) true
-	 */
-	readable?: boolean;
-
-	/**
-	 * Will the file be writable? (default) false
-	 */
-	writable?: boolean
-}
-
 export class FMODLoader extends Loader {
-	system: IFMOD.StudioSystem;
-	core: IFMOD.System;
+	private system: IFMOD.StudioSystem;
+	private core: IFMOD.System;
 
-	assetBaseURL: string;
-	readonly totalMemory: number;
-	private preloadData: FMODPreloadFileData[] = [];
+	// Events
+	readonly events = new DelegateGroup<
+	'preload' | 'runtimeinit' | 'banksloaded' | 'testsoundended', () => void>();
+	readonly onLoadFinish = new Delegate<(system: IFMOD.StudioSystem) => void>();
 
-	onFMODPreload = new Delegate<() => void>();
-	onFMODRuntimeInit = new Delegate<() => void>();
-	onBanksLoaded = new Delegate<() => void>();
-	onTestSoundEnded = new Delegate<() => void>();
-	onLoadFinish = new Delegate<(system: IFMOD.StudioSystem) => void>();
-
-	constructor(memory: number, assetBaseURL: string) {
+	constructor(private config: FMODLoaderConfig) {
 		super();
-		this.totalMemory = memory;
-		this.assetBaseURL = assetBaseURL;
+		// Handle config defaults
+		this.handleFMODLoaderConfigDefaults(config);
 
-		// attach events
-		this.onFMODPreload.subscribe(this, () => {this.preloadEvent(this.preloadData)});
-		this.onFMODRuntimeInit.subscribe(this, () => {this.runtimeInitializedEvent();});
-		this.onBanksLoaded.subscribe(this, () => {this.testSoundEvent();});
-		this.onTestSoundEnded.subscribe(this, () => {this.finishEvent();});
+		// Attach events
+		this.events
+			.on('preload', this, () => {this.evPreload(this.config.preloadFileData);})
+			.on('runtimeinit', this, () => {this.evRuntimeInitialized();})
+			.on('banksloaded', this, () => {this.evTestSound();})
+			.on('testsoundended', this, () => {this.evFinish();});
 	}
 
 	load() {
-		FMOD.TOTAL_MEMORY = this.totalMemory;
-		FMOD.preRun = () => {this.onFMODPreload.send();};
-		FMOD.onRuntimeInitialized = () => {this.onFMODRuntimeInit.send();};
+		FMOD.TOTAL_MEMORY = this.config.totalMemory;
+		FMOD.preRun = () => {this.events.send('preload');};
+		FMOD.onRuntimeInitialized = () => {this.events.send('runtimeinit');};
 		FMODModule(FMOD);
 	}
 
 	addPreloadFiles(data: FMODPreloadFileData) {
-		this.preloadData.push(data);
+		this.config.preloadFileData.push(data);
 	}
 
-
-	private preloadEvent = (configs: FMODPreloadFileData[]) => {
+	// ====================== EVENT HANDLERS ==========================
+	/**
+	 * Processes each FMODPreloadFileData config via IFMOD.FS_createPreloadedFile
+	 */
+	private evPreload = (configs: FMODPreloadFileData[]) => {
 		console.log('FMOD is Mounting Preload files...');
 		configs.forEach((data) => {
 			let url = data.url? data.url : '';
 			let readable = data.readable? data.readable : true;
 			let writable = data.writable? data.writable : false;
 			data.fileNames.forEach((filename) => {
-				IFMOD.FS_createPreloadedFile(data.directory, filename, this.assetBaseURL + '/' + url + filename, readable, writable);
+				IFMOD.FS_createPreloadedFile(data.directory, filename, this.config.assetBaseURL + '/' + url + filename, readable, writable);
 			});
 		});		
 	}
 
-	private runtimeInitializedEvent = () => {
+	/**
+	 * Creates the FMOD Studio System object, initializes it, and loads banks, sending the signal when loaded
+	 */
+	private evRuntimeInitialized = () => {
 		// TODO: Have data passed configurgurably with config object
 		console.log('FMOD Runtime has been initialized. Loading banks...');
 		let outval: any = {};
@@ -99,26 +67,43 @@ export class FMODLoader extends Loader {
 		this.system = outval.val as IFMOD.StudioSystem;
 		CHECK_RESULT(this.system.getCoreSystem(outval), 'getting core system');
 		this.core = outval.val as IFMOD.System;
-		CHECK_RESULT(this.system.initialize(128, IFMOD.STUDIO_INITFLAGS.NORMAL, IFMOD.INITFLAGS.NORMAL, null), 'initializing FMOD Studio');
-		
-		// Load banks
-		this.system.loadBankFile('Master Bank.bank', IFMOD.STUDIO_LOAD_BANK_FLAGS.NORMAL, outval);
-		this.system.loadBankFile('Master Bank.strings.bank', IFMOD.STUDIO_LOAD_BANK_FLAGS.NORMAL, outval);
+		CHECK_RESULT(this.system.initialize(this.config.maxChannels, this.config.studioInitFlags, this.config.initFlags, null), 'initializing FMOD Studio'); 
+		// Audio workaround can work once core is available.
+		this.setListenerAudioWorkaround();
 
-		// Set bank loaded check. TODO: Make this event just accept Bank[]
+		let banks: IFMOD.Bank[] = [];
+		// Load banks. TODO: Make a function that receives the config file and spits out Bank[]
+		this.config.initLoadBanks.forEach((bankData) => {
+			bankData.names.forEach((bankName) => {
+				let flags = bankData.flags? bankData.flags : IFMOD.STUDIO_LOAD_BANK_FLAGS.NORMAL;
+				CHECK_RESULT(this.system.loadBankFile(bankName, flags, outval), 'loading bank: ' + bankName);
+				banks.push(outval.val);
+			});
+		});
+
+		// Set bank loaded check
 		let bankCheckInterval = setInterval(() => {
-			if (this.checkBanksLoaded(this.system)) {
-				this.onBanksLoaded.send();
+			if (this.checkBanksLoaded(banks)) {
+				this.events.send('banksloaded');
 				clearInterval(bankCheckInterval);
 			}
 		}, 100);
 	}
 
-	private testSoundEvent = () => {
+	/**
+	 * Begins running the test sound (which should be silent). At this point the user may not have interacted with the screen yet.
+	 * Loading will halt here until that happens. Sends the signal when this sound has finished playing.
+	 */
+	private evTestSound = () => {
 		console.log('FMOD is testing sound');
-		// TODO: Configurable testeventname via GUID or EventName (flag for isGUID in config)
 		let outval: any = {};
-		CHECK_RESULT(this.system.getEvent('event:/Silence', outval), 'getting test event description');
+		let key = this.config.soundTestEvent;
+		if (key.includes('event:/')) {
+			CHECK_RESULT(this.system.getEvent(key, outval), 'getting test event description via stringbank key');
+		} else {
+			CHECK_RESULT(this.system.getEventByID(parseGUID(key), outval), 'getting test event description via ID')
+		}
+
 		let desc = outval.val as IFMOD.EventDescription;
 		CHECK_RESULT(desc.createInstance(outval), 'creating test event instance');
 		let inst = outval.val as IFMOD.EventInstance;
@@ -126,13 +111,24 @@ export class FMODLoader extends Loader {
 
 		let testSoundEndedInterval = setInterval(() => {
 			if (this.checkSoundEnded(inst)) {
-				this.onTestSoundEnded.send();
+				this.events.send('testsoundended');
 				clearInterval(testSoundEndedInterval);
-				inst.release();
+				inst.release(); // destroy this EventInstance from FMOD
 			}
-		}, 100);
+		}, 100); // check every 100 ms
 	}
 
+	private evFinish = () => {
+		console.log('FMOD has finished loading!');
+		this.events.destroy();
+
+		this.setListenerVisibilityChange();
+
+		this.onLoadFinish.send(this.system);
+		this.onLoadFinish.unsubscribeAll();
+	}
+
+	// ================== CHECKING (inside setInterval) ==================
 	private checkSoundEnded = (instance: IFMOD.EventInstance): boolean => {
 		this.system.update();
 		let outval: any = {};
@@ -141,10 +137,8 @@ export class FMODLoader extends Loader {
 		return state === IFMOD.STUDIO_PLAYBACK_STATE.STOPPED;
 	}
 
-	private checkBanksLoaded = (system: IFMOD.StudioSystem): boolean => {
+	private checkBanksLoaded = (banks: IFMOD.Bank[]): boolean => {
 		let outval: any = {};
-		system.getBankList(outval, 10, null);
-		let banks: IFMOD.Bank[] = outval.val;
 		let areBanksLoaded = true;
 		banks.forEach((bank) => {
 			bank.getLoadingState(outval);
@@ -156,13 +150,9 @@ export class FMODLoader extends Loader {
 		return areBanksLoaded;
 	}
 
-	private finishEvent = () => {
-		console.log('FMOD has finished loading!');
-		this.onFMODPreload.unsubscribeAll();
-		this.onFMODRuntimeInit.unsubscribeAll();
-		this.onBanksLoaded.unsubscribeAll();
-		this.onTestSoundEnded.unsubscribeAll();
-		
+	// ==================== Set Window Listeners ===========================
+	private setListenerVisibilityChange() {
+		// Event to suspend audio when window is not visible and resume when it is
 		window.addEventListener('visibilitychange', (ev) => {
 			if (document.hidden) {
 				CHECK_RESULT(this.core.mixerSuspend(), 'suspending core mixer');
@@ -170,15 +160,26 @@ export class FMODLoader extends Loader {
 				CHECK_RESULT(this.core.mixerResume(), 'resuming core mixer');
 			}
 		});
+	}
 
-		let audioResumeEvent = 'pointerdown';
+	private setListenerAudioWorkaround() {
+		// Set chrome/ios workaround (gesture must happen before sound plays)
+		let audioResumeEvent = 'click';
 		let audioContextResume = (ev) => {
+			CHECK_RESULT(this.core.mixerSuspend(), 'suspending core mixer');
 			CHECK_RESULT(this.core.mixerResume(), 'resuming core mixer');
-			console.log('Resuming mixer.');
+			console.log('Resuming mixer.', ev);
 			window.removeEventListener(audioResumeEvent, audioContextResume);
 		}
 		window.addEventListener(audioResumeEvent, audioContextResume);
-		
-		this.onLoadFinish.send(this.system);
+	}
+
+	// ===================== UTILITY =======================================
+	private handleFMODLoaderConfigDefaults(config: FMODLoaderConfig) {
+		config.initLoadBanks = config.initLoadBanks? config.initLoadBanks : [];
+		config.preloadFileData = config.preloadFileData? config.preloadFileData : [];
+		config.maxChannels = config.maxChannels? config.maxChannels : 128;
+		config.studioInitFlags = config.studioInitFlags? config.studioInitFlags : IFMOD.STUDIO_INITFLAGS.NORMAL;
+		config.initFlags = config.initFlags? config.initFlags : IFMOD.INITFLAGS.NORMAL;
 	}
 }
