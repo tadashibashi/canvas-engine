ba8fe23ee7db4cd36b11c610cb684b33824b43d4
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-09-22 15:58:37 -0700

    Clean up code in ComponentManager

--- src/engine/ComponentManager.ts
+++ src/engine/ComponentManager.ts
@@ -9,16 +9,16 @@ import { ArrayMap } from "./utility/ArrayMap";
  * @type T Constrains the type of Component to be managed. Default: Component
  */
 export class ComponentManager<T extends Component = Component> extends DrawableComponent {
-    private components: T[] = [];
+  private components: T[] = [];
 	private drawList: DrawableComponent[] = [];
-    private activated = false;
+  private activated = false;
 	onAdded = new Delegate<(component: T) => void>();
 	onRemoved = new Delegate<(component: T) => void>();
 
     constructor(tag?: string | null, updateOrder = 0, drawOrder = 0) {
-        super(tag, updateOrder, drawOrder);
-        this.onAdded.subscribe((component) => { this.tagBank.addToArray(component.tag, component)}, this);
-        this.onRemoved.subscribe((component) => {this.tagBank.removeFromArray(component.tag, component)}, this);
+			super(tag, updateOrder, drawOrder);
+			this.onAdded.subscribe((component) => { this.tagBank.addToArray(component.tag, component)}, this);
+			this.onRemoved.subscribe((component) => {this.tagBank.removeFromArray(component.tag, component)}, this);
     }
 	// ============ COMPONENT MANAGEMENT (add, get, remove, sort) ============= //
 	/**
@@ -36,34 +36,34 @@ export class ComponentManager<T extends Component = Component> extends DrawableC
 		throw new Error('ComponentManager does not contain a Component of type: ' + type.name + ', which was attempted to be retrieved via get()' );
     }
     get length(): number {
-        return this.components.length;
+			return this.components.length;
 	}
 	private tagBank = new ArrayMap<string, T>(true);
 	
-    /**
-     * This provides you with a reference to an array of all like-tagged items, which will expand 
-     * and delete automatically as elements are pushed and spliced. If there is no array existing
-     * for the indicated tag, a new one will be created for that tag, to be populated in the future.
-     * @param tag The tag of the array to find
-     */
-    getByTag(tag: string): T[] {
-        return this.tagBank.get(tag);
-    }
+	/**
+	 * This provides you with a reference to an array of all like-tagged items, which will expand 
+	 * and delete automatically as elements are pushed and spliced. If there is no array existing
+	 * for the indicated tag, a new one will be created for that tag, to be populated in the future.
+	 * @param tag The tag of the array to find
+	 */
+	getByTag(tag: string): T[] {
+		return this.tagBank.get(tag);
+	}
 
-    /**
-     * Gets all components of a specific type inside ComponentManager, at any given time
-     * This creates a new array that is not a reference of any array stored in this manager.
-     * @param type
-     */
-    getAllOfType<C extends T>(type: new (...any: any[]) => C): C[] {
-        const components = this.components;
-        const componentsOfType: C[] = [];
-        for (let i = 0; i < components.length; i++) {
-            if (components[i] instanceof type) {
-                componentsOfType.push(components[i] as C);
-            }
-        }
-        return componentsOfType;
+	/**
+	 * Gets all components of a specific type inside ComponentManager, at any given time
+	 * This creates a new array that is not a reference of any array stored in this manager.
+	 * @param type
+	 */
+	getAllOfType<C extends T>(type: new (...any: any[]) => C): C[] {
+			const components = this.components;
+			const componentsOfType: C[] = [];
+			for (let i = 0; i < components.length; i++) {
+				if (components[i] instanceof type) {
+					componentsOfType.push(components[i] as C);
+				}
+			}
+			return componentsOfType;
 	}
 
 	/**
@@ -88,10 +88,10 @@ export class ComponentManager<T extends Component = Component> extends DrawableC
 	 */
 	add<C extends T>(component: C) {
 		if (this.components.indexOf(component) >= 0) {
-			console.log('Warning. This ComponentManager already contains this Component. Aborting add function.');
-        } else {
-            // Cache ComponentManager to Component to enable access to other components
-            component.manager = this;
+			if (this.isDebug) console.log('Warning. This ComponentManager already contains this Component. Aborting add function.');
+		} else {
+			// Cache ComponentManager to Component to enable access to other components
+			component.manager = this;
 			// Add component to the appropriate arrays, and add listeners for on order changed.
 			if (component instanceof DrawableComponent) {
 				this.drawList.unshift(component);
@@ -101,9 +101,9 @@ export class ComponentManager<T extends Component = Component> extends DrawableC
 			component.onUpdateOrderChanged.subscribe(this.sortUpdateListOrder, this);
 			this.components.unshift(component);
 			
-            if (this.activated) {// after scene/game start will auto awake newly created component
-                component.awake();
-            }
+			if (this.activated) {// after scene/game start will auto awake newly created component
+				component.awake();
+			}
 			this.onAdded.send(component);
 		}
 		return this;	
