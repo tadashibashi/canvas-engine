8f724cca5de5c85aa8d6cf8043b50479cade8297
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2019-09-22 02:43:09 -0700

    Setup Webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+  mode: 'production',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: [/node_modules/, /libraries/]
+      }
+    ]
+  },
+  watch: true,
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: false,
+          keep_classnames: undefined,
+          keep_fnames: false,
+          topLevel: true
+        }
+      })
+    ]
+  }
+};
